6d1e9033bb9531efcb7562a803f4bf06af41fe9b
rustiandacode <rustiandazen09@gmail.com>
2022-12-11 20:40:31 +0700

    added navbar function that can change background color on scroll web

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
-function scrollNavbar() {
-  let calc = window.scrollY;
+// change navbar background on scroll web
+function navbarScroll() {
+  let calc = scrollY;
   let navbar = document.querySelector('nav');
+
   if (calc > 0) {
     navbar.classList.add('navbar-scroll');
   } else if (calc <= 0) {
     navbar.classList.remove('navbar-scroll');
   }
+
+  console.log(calc);
 }
 
 window.onscroll = () => {
-  scrollNavbar();
+  navbarScroll();
 };
